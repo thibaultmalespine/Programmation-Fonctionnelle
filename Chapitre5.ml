7e7353b34a1e5885e7d78aca51397ef487d82d8c
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2024-09-27 18:04:45 +0200

    fin exercice 3 chapitre 5

--- Chapitre5.ml
+++ Chapitre5.ml
@@ -1,13 +1,40 @@
 (* Exercice 1 *)
 
 let rec somme = function
-  1 -> 1 |  
+  0 -> 0 |  
   n -> n + somme(n-1);;
 
 
 (* Exercice 2 *)
 
 let rec puissance = function 
-  (x, 1) -> x |
+  (x, 0) -> 1 |
   (x, p) -> x * puissance(x, p-1);;
   
+
+(* Bonus *)
+
+let rec factorielle = function  
+  0 -> 1 |
+  x -> x * factorielle(x-1);;
+
+(* Exercice 3 *)
+let rec repet = function
+  c,0 -> 0 |
+  c,n -> c * puissance(10,n-1) + repet(c, n-1);;  
+
+let rec unChiffre = function(n,c) ->
+  if (n = c) then true else if (n < c) then false
+  else (n mod 10) = c && unChiffre(n/10,c);;
+
+let rec pgd = function
+  n,1 -> 1 |
+  n,d -> if (n mod d) = 0 then d else pgd(n, d-1);;
+  
+let nbPairChif = function(n) -> 
+  let rec nbChif = function(n) ->  
+    if n < 10 then 1
+    else 1 + nbChif(n/10)
+  in    
+    (nbChif(n) mod 2) = 0;;
+    
